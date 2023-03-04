799c7d6377dcaa4acf9a43fbd83eff9091db72d9
Eric <37033468+ebrist@users.noreply.github.com>
2023-03-04 17:27:24 -0500

    Update init.R

--- init.R
+++ init.R
@@ -1,7 +1,6 @@
 # init.R
 my_packages = c("stringr", "data.table", "dplyr", "shiny",
-                "shinyWidgets", "DT", "waiter", "shinythemes",
-                "rcorpora", "lexicon")
+                "shinyWidgets", "DT", "waiter", "shinythemes")
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
