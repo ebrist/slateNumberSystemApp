f8148ba9e47c4091b77de012ee3a23b4cd45fbb7
Eric <37033468+ebrist@users.noreply.github.com>
2023-03-05 20:07:56 -0500

    Add files via upload

--- ui.R
+++ ui.R
@@ -23,113 +23,108 @@ fluidPage(
       tabPanel(
         title = 'Slate System', 
         value = 'system',
-        column(
-          width = 10,
-          offset = 1,
-          align = 'left',
-          fluidRow(
-            style = "margin-top:5px;",
-            div(style="display:inline-block;vertical-align:top;", tags$p(style = "margin-top:8px;margin-right:5px;", 'Enter a number:')),
+        fluidRow(
+          style = "margin-top:5px;",
+          div(style="display:inline-block;vertical-align:top;", tags$p(style = "margin-top:8px;margin-right:5px;", 'Enter a number:')),
+          div(
+            style = "display:inline-block;vertical-align:top;",
             div(
               style = "display:inline-block;vertical-align:top;",
-              div(
-                style = "display:inline-block;vertical-align:top;",
-                div(style="display:inline-block;vertical-align:top;margin-bottom:-10px;", uiOutput("input_number_ui")),
-                div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('convert', 'Convert', width = '80px'))  
-              ),
-              div(
-                style="display:inline-block;vertical-align:top;",
-                div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('convert_previous', 'Previous', width = '80px')),
-                div(style="display:inline-block;vertical-align:top;padding-bottom:4px;padding-right:7px;", actionButton('convert_next', 'Next', width = '80px'))
-              )
+              div(style="display:inline-block;vertical-align:top;margin-bottom:-10px;", uiOutput("input_number_ui")),
+              div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('convert', 'Convert'))  
             ),
             div(
-              class = 'dark-theme-toggle',
-              style="display:inline-block;vertical-align:top;margin-bottom:0px;margin-bottom:10px;margin-top:8px;", 
-              materialSwitch(
-                inputId = "stress",
-                label = "Stress Markers", 
-                value = FALSE,
-                status = "info"
-              )
-            )
-          ),
-          fluidRow(
-            uiOutput('error')
-          ),
-          div(
-            style = "padding-bottom: 10px;",
-            uiOutput('phonemes'),
-          ),
-          fluidRow(
-            div(style="display:inline-block;vertical-align:top;margin-top:8px;font-size:15px;margin-right:10px;margin-bottom:15px;", uiOutput('results_info')),
-            div(
-              style="display:inline-block;vertical-align:top;font-size:15px;margin-right:10px;", 
-              div(
-                style='display:flex;flex-direction:row;flex-wrap:wrap;gap:5px;',
-                div(style="margin-top:8px", tags$p('Show')),
-                div(style="margin-bottom:-25px", uiOutput('page_length_ui')),
-                div(style="margin-top:8px;margin-bottom:20px;", tags$p('results per page.'))
-              )
-            ),
-            div(
-              style="display:inline-block;vertical-align:top;font-size:15px;float:right;", 
-              div(
-                style = 'display:flex;flex-direction:row;flex-wrap:wrap;gap:5px;',
-                div(style="display:inline-block;vertical-align:top;margin-top:8px;margin-right:5px;padding-bottom:4px;", uiOutput('page_number_info')), 
-                div(
-                  style="display:inline-block;vertical-align:",
-                  div(
-                    style="display:inline-block;vertical-align:",
-                    div(style="display:inline-block;vertical-align:top;margin-bottom:-15px;padding-bottom:4px;", uiOutput('page_number_ui')),
-                    div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('go', 'Search', width = '80px'))
-                  ),
-                  div(
-                    style="display:inline-block;vertical-align:",
-                    div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('previous_page', 'Previous', width = '80px')), 
-                    div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('next_page', 'Next', width = '80px'))   
-                  )
-                )
-              )
-            )
-          ),
-          fluidRow(style = 'min-height: 370px;', DT::DTOutput('table')),
-          fluidRow(
-            style = "padding-top: 15px;",
-            div(style="display:inline-block;vertical-align:top;margin-top:8px;font-size:15px;margin-right:10px;margin-bottom:15px;", uiOutput('results_info2')),
-            div(
-              style="display:inline-block;vertical-align:top;font-size:15px;margin-right:10px;", 
-              div(
-                style='display:flex;flex-direction:row;flex-wrap:wrap;gap:5px;',
-                div(style="margin-top:8px", tags$p('Show')),
-                div(style="margin-bottom:-25px", uiOutput('page_length_ui2')),
-                div(style="margin-top:8px;margin-bottom:20px;", tags$p('results per page.'))
-              )
-            ),
-            div(
-              style="display:inline-block;vertical-align:top;font-size:15px;float:right;", 
-              div(
-                style = 'display:flex;flex-direction:row;flex-wrap:wrap;gap:5px;',
-                div(style="display:inline-block;vertical-align:top;margin-top:8px;margin-right:5px;padding-bottom:4px;", uiOutput('page_number_info2')), 
-                div(
-                  style="display:inline-block;vertical-align:",
-                  div(
-                    style="display:inline-block;vertical-align:",
-                    div(style="display:inline-block;vertical-align:top;margin-bottom:-15px;padding-bottom:4px;", uiOutput('page_number_ui2')),
-                    div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('go2', 'Search', width = '80px'))
-                  ),
-                  div(
-                    style="display:inline-block;vertical-align:",
-                    div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('previous_page2', 'Previous', width = '80px')), 
-                    div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('next_page2', 'Next', width = '80px'))   
-                  )
-                )
-              )
+              style="display:inline-block;vertical-align:top;",
+              div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('convert_previous', 'Previous')),
+              div(style="display:inline-block;vertical-align:top;padding-bottom:4px;padding-right:7px;", actionButton('convert_next', 'Next'))
             )
           ),
           div(
-            style = 'height: 200px;'
+            class = 'dark-theme-toggle',
+            style="display:inline-block;vertical-align:top;margin-bottom:0px;margin-bottom:10px;margin-top:8px;", 
+            materialSwitch(
+              inputId = "stress",
+              label = "Stress Markers", 
+              value = FALSE,
+              status = "info"
+            )
           )
+        ),
+        fluidRow(
+          uiOutput('error')
+        ),
+        div(
+          style = "padding-bottom: 10px;",
+          uiOutput('phonemes'),
+        ),
+        fluidRow(
+          div(style="display:inline-block;vertical-align:top;margin-top:8px;font-size:15px;margin-right:10px;margin-bottom:15px;", uiOutput('results_info')),
+          div(
+            style="display:inline-block;vertical-align:top;font-size:15px;margin-right:10px;", 
+            div(
+              style='display:flex;flex-direction:row;flex-wrap:wrap;gap:5px;',
+              div(style="margin-top:8px", tags$p('Show')),
+              div(style="margin-bottom:-25px", uiOutput('page_length_ui')),
+              div(style="margin-top:8px;margin-bottom:20px;", tags$p('results per page.'))
+            )
+          ),
+          div(
+            style="display:inline-block;vertical-align:top;font-size:15px;float:right;", 
+            div(
+              style = 'display:flex;flex-direction:row;flex-wrap:wrap;gap:5px;',
+              div(style="display:inline-block;vertical-align:top;margin-top:8px;margin-right:5px;padding-bottom:4px;", uiOutput('page_number_info')), 
+              div(
+                style="display:inline-block;vertical-align:",
+                div(
+                  style="display:inline-block;vertical-align:",
+                  div(style="display:inline-block;vertical-align:top;margin-bottom:-15px;padding-bottom:4px;", uiOutput('page_number_ui')),
+                  div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('go', 'Search'))
+                ),
+                div(
+                  style="display:inline-block;vertical-align:",
+                  div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('previous_page', 'Previous')), 
+                  div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('next_page', 'Next'))   
+                )
+              )
+            )
+          )
+        ),
+        fluidRow(style = 'min-height: 370px;', DT::DTOutput('table')),
+        fluidRow(
+          style = "padding-top: 15px;",
+          div(style="display:inline-block;vertical-align:top;margin-top:8px;font-size:15px;margin-right:10px;margin-bottom:15px;", uiOutput('results_info2')),
+          div(
+            style="display:inline-block;vertical-align:top;font-size:15px;margin-right:10px;", 
+            div(
+              style='display:flex;flex-direction:row;flex-wrap:wrap;gap:5px;',
+              div(style="margin-top:8px", tags$p('Show')),
+              div(style="margin-bottom:-25px", uiOutput('page_length_ui2')),
+              div(style="margin-top:8px;margin-bottom:20px;", tags$p('results per page.'))
+            )
+          ),
+          div(
+            style="display:inline-block;vertical-align:top;font-size:15px;float:right;", 
+            div(
+              style = 'display:flex;flex-direction:row;flex-wrap:wrap;gap:5px;',
+              div(style="display:inline-block;vertical-align:top;margin-top:8px;margin-right:5px;padding-bottom:4px;", uiOutput('page_number_info2')), 
+              div(
+                style="display:inline-block;vertical-align:",
+                div(
+                  style="display:inline-block;vertical-align:",
+                  div(style="display:inline-block;vertical-align:top;margin-bottom:-15px;padding-bottom:4px;", uiOutput('page_number_ui2')),
+                  div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('go2', 'Search'))
+                ),
+                div(
+                  style="display:inline-block;vertical-align:",
+                  div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('previous_page2', 'Previous')), 
+                  div(style="display:inline-block;vertical-align:top;padding-bottom:4px;", actionButton('next_page2', 'Next'))   
+                )
+              )
+            )
+          )
+        ),
+        div(
+          style = 'height: 200px;'
         )
       ),
       tabPanel(
